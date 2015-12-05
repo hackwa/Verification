44361b28e337121861847cbf595ef0b8564050e0
Anurag Dubey <anurag.dubey@colorado.edu>
2015-12-05 14:39:32 -0700

    backlist global

--- solver.py
+++ solver.py
@@ -20,27 +20,27 @@ class incrementalSolver(SATRep):
         return wlist
         print(wlist)
 
-    def eliminate_ptr_to(self,n,wlist,blacklist):
+    def eliminate_ptr_to(self,n,wlist):
         clause = self.sentence[n]
         for i in range(len(clause)):
-            if clause[i] not in blacklist:
+            if clause[i] not in self.blacklist:
                 wlist[n] = clause[i]
                 return True
         return False
 
     # Conflict detection happens here
-    def _update_watch(self,eliminate,wlist,blacklist):
+    def _update_watch(self,eliminate,wlist):
         ret = True
         for i in range(self.nclauses):
             if wlist[i] == eliminate:
-                ret = self.eliminate_ptr_to(i,wlist,blacklist)
+                ret = self.eliminate_ptr_to(i,wlist)
                 if ret is False:
                     print("Conflict!")
                     break
         return ret
 
 
-    def _recurse(self,depth,wlist,blacklist):
+    def _recurse(self,depth,wlist):
         if depth == len(self.variables):
             self.satisfied = 1
             print("Satisfied!")
@@ -49,22 +49,19 @@ class incrementalSolver(SATRep):
         self.stack.append([var,None])
         code = self.var_map[var]
         for truth in [True, False]:
-            if truth is True:
-                eliminate = code << 1 | 1            
-            elif truth is False:
-                eliminate  = code << 1 | 0
-            blacklist.append(eliminate)
+            eliminate  = code << 1 | (1 & truth)
+            self.blacklist.append(eliminate)
             print("Setting %s to %s"%(var,truth))
             self.stack[-1][1] = truth
             print(self.stack)
-            print(wlist,blacklist,eliminate)
-            if self._update_watch(eliminate,wlist,blacklist):
-                if not self._recurse(depth+1,wlist,blacklist):
-                    blacklist.pop()
+            print(wlist,self.blacklist,eliminate)
+            if self._update_watch(eliminate,wlist):
+                if not self._recurse(depth+1,wlist):
+                    self.blacklist.pop()
                 else:
                     return True
             else:
-                blacklist.pop()
+                self.blacklist.pop()
         self.stack.pop()
         return False
 
@@ -72,9 +69,9 @@ class incrementalSolver(SATRep):
     def  solve(self):
         wlist = self._init_watch()
         self.stack = []
-        blacklist = []
+        self.blacklist = []
         depth = 0
-        self.satisfies = self._recurse(depth,wlist,blacklist)
+        self.satisfies = self._recurse(depth,wlist)
 
 def main():
     ap = argparse.ArgumentParser()
